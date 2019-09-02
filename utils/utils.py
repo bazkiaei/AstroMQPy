10c32901cdb6b8679ccfc81c83e59544f045128a
Amir <amir.ebadati-bazkiaei@students.mq.edu.au>
2019-09-02 13:19:05 +1000

    trying to polish the first function

---
+++ utils/utils.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+import logbook
+
+import sys
+
+
+logbook.StreamHandler(sys.stdout).push_application()
+logger = logbook.Logger('logbook')
+
+
+def pixel_distance(stars):
+    """
+    This function measures the distance between to objects. First, it was
+    designed to measure the distance betweeen stars in an image in units of
+    pixels
+
+    Parameters
+    ----------
+    stars : np.array
+        An array representing the positions of the stars.
+
+    Returns
+    -------
+    np.array
+        Array which contains the distances between stars.
+    """
+    distances = np.zeros((stars.shape[0], stars.shape[0]))
+    logger.info('Start')
+    for i in range(stars.shape[0]):
+        if i < stars.shape[0] - 1:
+            dis1 = stars[i, 0] - stars[i + 1:, 0]
+            dis2 = stars[i, 1] - stars[i + 1:, 1]
+            dis = np.sqrt(dis1 ** 2 + dis2 ** 2)
+            distances[i][i + 1:] = dis
+    logger.info('The End!')
+    return distances
